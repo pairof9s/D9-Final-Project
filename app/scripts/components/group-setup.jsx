f9fb0236b574bca5ac92a869e1b54c1c5aa175de
David Nine <pairof9s@me.com>
2016-07-23 17:58:38 -0400

    Sat edits and additions to Group page

--- app/scripts/components/group-setup.jsx
+++ app/scripts/components/group-setup.jsx
@@ -8,28 +8,20 @@ var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
 var StudentCollection = require('../models/students').StudentCollection;
 
 
-var cols = [
-    { type: 'studentName', label: 'Eligible Students' },
-    { type: 'studentSchool', label: 'School' },
-    { type: 'studentGrade', label: 'Grade Level' },
-    { type: 'studentGender', label: 'Gender' },
-    { type: 'selectStudent', label: 'Select'}
-];
-
 var data = [
-    { id: 4021, studentName: 'Watt Ever', studentSchool: 'Beck Middle School', studentGrade: '7', studentGender: 'M', selectStudent: <input type="checkbox"/> },
-    { id: 4022, studentName: 'Jane Plane', studentSchool: 'Beck Middle School', studentGrade: '7', studentGender: 'F', selectStudent: <input type="checkbox"/> },
-    { id: 4023, studentName: 'Sally Mow', studentSchool: 'Beck Middle School', studentGrade: '7', studentGender: 'F', selectStudent: <input type="checkbox"/> },
-    { id: 4024, studentName: 'Tye Neetim', studentSchool: 'Beck Middle School', studentGrade: '8', studentGender: 'M', selectStudent: <input type="checkbox"/> },
-    { id: 4025, studentName: 'Wanda Full', studentSchool: 'Beck Middle School', studentGrade: '7', studentGender: 'F', selectStudent: <input type="checkbox"/> },
-    { id: 4026, studentName: 'Ben Dumm', studentSchool: 'Beck Middle School', studentGrade: '6', studentGender: 'M', selectStudent: <input type="checkbox"/> },
-    { id: 4027, studentName: 'Tom Mee', studentSchool: 'Beck Middle School', studentGrade: '8', studentGender: 'M', selectStudent: <input type="checkbox"/> },
-    { id: 4028, studentName: 'Deb Utawnt', studentSchool: 'Beck Middle School', studentGrade: '7', studentGender: 'F', selectStudent: <input type="checkbox"/> },
-    { id: 4029, studentName: 'Jack Spratt', studentSchool: 'Beck Middle School', studentGrade: '7', studentGender: 'M', selectStudent: <input type="checkbox"/> },
-    { id: 4030, studentName: 'Joe Kerr', studentSchool: 'Beck Middle School', studentGrade: '6', studentGender: 'M', selectStudent: <input type="checkbox"/> },
-    { id: 4031, studentName: 'Robin DeRitch', studentSchool: 'Beck Middle School', studentGrade: '7', studentGender: 'F', selectStudent: <input type="checkbox"/> },
-    { id: 4032, studentName: 'Sally Mander', studentSchool: 'Beck Middle School', studentGrade: '8', studentGender: 'F', selectStudent: <input type="checkbox"/> },
-    { id: 4033, studentName: 'Frank Lee', studentSchool: 'Beck Middle School', studentGrade: '7', studentGender: 'M', selectStudent: <input type="checkbox"/> },
+    {id: 4021, studentName: 'Watt Ever', studentSchool: 'Beck Middle School', studentGrade: '7', studentGender: 'M'},
+    {id: 4022, studentName: 'Jane Plane', studentSchool: 'Beck Middle School', studentGrade: '7', studentGender: 'F'},
+    {id: 4023, studentName: 'Sally Mow', studentSchool: 'Beck Middle School', studentGrade: '7', studentGender: 'F'},
+    {id: 4024, studentName: 'Tye Neetim', studentSchool: 'Beck Middle School', studentGrade: '8', studentGender: 'M'},
+    {id: 4025, studentName: 'Wanda Full', studentSchool: 'Beck Middle School', studentGrade: '7', studentGender: 'F'},
+    {id: 4026, studentName: 'Ben Dumm', studentSchool: 'Beck Middle School', studentGrade: '6', studentGender: 'M'},
+    {id: 4027, studentName: 'Tom Mee', studentSchool: 'Beck Middle School', studentGrade: '8', studentGender: 'M'},
+    {id: 4028, studentName: 'Deb Utawnt', studentSchool: 'Beck Middle School', studentGrade: '7', studentGender: 'F'},
+    {id: 4029, studentName: 'Jack Spratt', studentSchool: 'Beck Middle School', studentGrade: '7', studentGender: 'M'},
+    {id: 4030, studentName: 'Joe Kerr', studentSchool: 'Beck Middle School', studentGrade: '6', studentGender: 'M'},
+    {id: 4031, studentName: 'Robin DeRitch', studentSchool: 'Beck Middle School', studentGrade: '7', studentGender: 'F'},
+    {id: 4032, studentName: 'Sally Mander', studentSchool: 'Beck Middle School', studentGrade: '8', studentGender: 'F'},
+    {id: 4033, studentName: 'Frank Lee', studentSchool: 'Beck Middle School', studentGrade: '7', studentGender: 'M'},
 ];
 
 var onRowSelect;
@@ -37,10 +29,10 @@ var onSelectAll;
 var selectRowProp = {
     mode: "checkbox",
     clickToSelect: true,
-    bgColor: "rgb(238, 193, 213)",
+    bgColor: "#FEB08E",
     onSelect: onRowSelect,
     onSelectAll: onSelectAll
-};
+  };
 
 var GroupSetup = React.createClass({
   getInitialState: function(){
@@ -49,21 +41,7 @@ var GroupSetup = React.createClass({
     };
   },
   componentWillMount: function(){
-    // var cols = this.state.cols;
-    console.log(cols);
-  },
-  generateHeaders: function() {
-    return cols.map(function(colData) {
-        return <th key={colData.type}>{colData.label}</th>;
-    });
-  },
-  generateRows: function() {
-    return data.map(function(item) {
-      var cells = cols.map(function(colData) {
-        return <td>{item[colData.type]}</td>;
-      });
-      return <tr key={item.id}>{cells}</tr>;
-    });
+    console.log(data);
   },
   onRowSelect: function(row, isSelected){
       console.log(row);
@@ -72,28 +50,34 @@ var GroupSetup = React.createClass({
   onSelectAll: function(isSelected){
       console.log("is select all: " + isSelected);
   },
+  onSubmit: function(){
+    e.preventDefault();
+    var router = this.props.router;
+  },
   render: function(){
     return (
       <div className="col-md-10 group-table">
         <div className="icon-third"><img src="images/PoolParty_logo.png"></img></div>
-        <div><h1>Welcome, David!</h1></div>
-        <div>
-          <p>This is copy that welcomes Primary/Secondary parent's name and the Student's name. Information concerning the Student's school & class level, their parent’s name, home address and email. The lower level will contain the current month’s schedule as well as a request board for schedule changes.</p>
+        <div><h1 id="page-title">Welcome, David!</h1></div>
+        <div className="sect-sep">
+          <p>Now that you're a Pool Party member, let's start with creating a Group. The Students you'll see listed below live within a 2-mile radius of your home. This makes it convenient for both them and you to join together.</p>
         </div>
-        <div className="row col-md-8">
+        <div className="row col-sm-9">
           <div className="panel panel-success">
             <div className="panel-heading"><h4>Create A Group!</h4></div>
               <div className="panel-body">
                 <p>Select from the eligible students shown to form your potential car pool group. Do not select more than 4 potential students; Groups cannot consist of more than 5 members. Students with the name crossed through have already joined a group.</p>
                 <p>Once you’ve selected your group, name the group below. Then click the Create button. An email will be sent to each student’s parent(s) informing them of your wish to have them join your car pool. You will be notified once they have confirmed their participation.</p>
               </div>
-          <div>
-            <BootstrapTable data={data} height="280" selectRow={selectRowProp}>
-              <TableHeaderColumn dataField="studentName" isKey={true} width="30%">Eligible Students</TableHeaderColumn>
-              <TableHeaderColumn dataField="studentSchool" dataAlign="center" width="40%">School</TableHeaderColumn>
-              <TableHeaderColumn dataField="studentGrade" dataAlign="center" width="10%">Grade Level</TableHeaderColumn>
-              <TableHeaderColumn dataField="studentGender" dataAlign="center" width="10%">Gender</TableHeaderColumn>
-            </BootstrapTable>
+              <div>
+                <BootstrapTable className="table" data={data} height="280" selectRow={selectRowProp} striped={true} hover={true} condensed={true}>
+                  <TableHeaderColumn dataField="studentName" isKey={true} dataSort={true} className="td-header-group">Students Eligible</TableHeaderColumn>
+                  <TableHeaderColumn dataField="studentSchool" dataAlign="center" className="td-header-group">School</TableHeaderColumn>
+                  <TableHeaderColumn dataField="studentGrade" dataAlign="center" className="td-header-group">Grade Level</TableHeaderColumn>
+                  <TableHeaderColumn dataField="studentGender" dataAlign="center" className="td-header-group">Gender</TableHeaderColumn>
+                </BootstrapTable>
+              </div>
+            </div>
             <div>
               <button type="submit" className="btn btn-success btn-sm pull-right">Create Group</button>
             </div>
@@ -101,10 +85,19 @@ var GroupSetup = React.createClass({
       </div>
     )
   }
-}),
+});
 
+var GroupSelect = React.createClass({
 
+  componentWillMount: function(){
+    var self = this;
+  },
+  render: function(){
+    return router;
+  }
+});
 
 module.exports = {
-  'GroupSetup': GroupSetup
-}
+  'GroupSetup': GroupSetup,
+  'GroupSelect': GroupSelect
+};
